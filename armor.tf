504a60849899ec9c00fa3dee50479657aa7f52ca
omkarm07 <mangalekaromkar@gmail.com>
2024-10-17 19:16:06 +0530

    Modulw

---
+++ armor.tf
@@ -0,0 +1,25 @@
+locals {
+  policy = yamldecode(file("${path.module}/cloud-armor.yaml"))
+}
+
+module "security_policy" {
+  source   = "./module/cloud-armor"
+
+  project_id                           = local.policy["project_id"]
+  name                                 = local.policy["name"]
+  description                          = local.policy["description"]
+  default_rule_action                  = local.policy["default_rule_action"]
+  type                                 = local.policy["type"]
+
+  # Pre-configured WAF Rules
+
+  pre_configured_rules = local.policy["pre_configured_rules"]
+  # Action against specific IP addresses or IP adress ranges
+
+  security_rules = local.policy["security_rules"]
+
+  # Custom Rules using CEL
+
+  custom_rules = local.policy["custom_rules"]
+
+}
